4b83220424494fc046cd218b265108e04048c743
DavinaCahyani <davinacahyaniputri@gmail.com>
2024-04-22 10:17:17 +0700

    update dashboard

--- src/pages/user/DashboardUser.js
+++ src/pages/user/DashboardUser.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "../../components/Sidebar";
+import Sidebar from "../../components/SidebarUser";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import {
@@ -7,7 +7,7 @@ import {
   faCircleXmark,
   faClockFour,
 } from "@fortawesome/free-regular-svg-icons";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/NavbarUser";
 
 function Dashboard() {
   return (
@@ -16,27 +16,27 @@ function Dashboard() {
         <Navbar />
       </div>
       <div className="flex h-full">
-        <div>
+        <div className="fixed">
           <Sidebar />
         </div>
-        <div className="content-page max-h-screen container p-8 min-h-screen bg-slate-200">
+        <div className="content-page container p-8 bg-slate-200 ml-0 md:ml-64 mt-12">
           <h1 className="judul text-3xl font-semibold text-center">
             Selamat Datang
           </h1>
           <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 mt-12">
-            <div className="pl-2 h-32 bg-green-400 rounded-lg shadow-md">
+            {/* Ubah class untuk lebar saat di mode desktop */}
+            <div className="pl-2 h-32 bg-green-400 rounded-lg shadow-md md:w-auto">
               <div className="flex w-full h-full py-2 px-4 bg-gray-100 rounded-lg justify-between">
                 <div className="my-auto">
                   <p className="font-bold">Masuk</p>
                   <p className="text-lg">Absen masuk.</p>
                 </div>
                 <div className="my-auto">
-                  <FontAwesomeIcon icon={faArrowRightFromBracket} size="2x" />{" "}
-                  {/* Menggunakan properti size untuk memperbesar ikon */}
+                  <FontAwesomeIcon icon={faArrowRightFromBracket} size="2x" />
                 </div>
               </div>
             </div>
-            <div className="pl-2 h-32 bg-red-400 rounded-lg shadow-md">
+            <div className="pl-2 h-32 bg-red-400 rounded-lg shadow-md md:w-auto">
               <div className="flex w-full h-full py-2 px-4 bg-gray-100 rounded-lg justify-between">
                 <div className="my-auto">
                   <p className="font-bold">Pulang</p>
@@ -48,7 +48,7 @@ function Dashboard() {
                 </div>
               </div>
             </div>
-            <div className="pl-2 h-32 bg-red-800 rounded-lg shadow-md">
+            <div className="pl-2 h-32 bg-red-800 rounded-lg shadow-md md:w-auto">
               <div className="flex w-full h-full py-2 px-4 bg-gray-100 rounded-lg justify-between">
                 <div className="my-auto">
                   <p className="font-bold">Izin</p>
@@ -61,11 +61,8 @@ function Dashboard() {
               </div>
             </div>
           </div>
-          <div className="flex justify-center mt-3 gap-4">
-            {" "}
-            <div className="pl-2 h-32 w-80 bg-blue-400 rounded-lg shadow-md mr-20">
-              {" "}
-              {/* Menggunakan w-80 untuk menyesuaikan lebar */}
+          <div className="flex justify-center mt-3 gap-4 flex-col md:flex-row">
+            <div className="pl-2 h-32 w-full md:w-80 bg-blue-400 rounded-lg shadow-md md:mr-20">
               <div className="flex w-full h-full py-2 px-4 bg-gray-100 rounded-lg justify-between">
                 <div className="my-auto">
                   <p className="font-bold">Cuti</p>
@@ -77,9 +74,7 @@ function Dashboard() {
                 </div>
               </div>
             </div>
-            <div className="pl-2 h-32 w-80 bg-yellow-400 rounded-lg shadow-md ml-20">
-              {" "}
-              {/* Menggunakan w-80 untuk menyesuaikan lebar */}
+            <div className="pl-2 h-32 w-full md:w-80 bg-yellow-400 rounded-lg shadow-md md:ml-0 mt-4 md:mt-0">
               <div className="flex w-full h-full py-2 px-4 bg-gray-100 rounded-lg justify-between">
                 <div className="my-auto">
                   <p className="font-bold">Lembur</p>
@@ -89,6 +84,35 @@ function Dashboard() {
                   <FontAwesomeIcon icon={faClockFour} size="2x" />{" "}
                   {/* Menggunakan properti size untuk memperbesar ikon */}
                 </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8 mt-12">
+            <div className="bg-green-400 rounded-lg shadow-md p-4 md:w-full lg:w-auto">
+              {" "}
+              {/* Ubah lebar saat di mode ponsel */}
+              <div className="flex justify-between items-center">
+                <div>
+                  <p className="text-white font-bold text-lg">Total Absen</p>
+                  <p className="text-white text-md">
+                    Jumlah absen yang tercatat
+                  </p>
+                </div>
+                <div className="text-white text-2xl font-semibold">100</div>
+              </div>
+            </div>
+            <div className="bg-red-800 rounded-lg shadow-md p-4 md:w-full lg:w-auto">
+              {" "}
+              {/* Ubah lebar saat di mode ponsel */}
+              <div className="flex justify-between items-center">
+                <div>
+                  <p className="text-white font-bold text-lg">Total Izin</p>
+                  <p className="text-white text-md">
+                    Jumlah izin yang diajukan
+                  </p>
+                </div>
+                <div className="text-white text-2xl font-semibold">2</div>
               </div>
             </div>
           </div>
