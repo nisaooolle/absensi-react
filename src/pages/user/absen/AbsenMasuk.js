53044b842ee4ee71cad5587dc9f5857e1d92516c
Keychan2725 <satriachandrapamungkas@gmail.com>
2024-05-13 10:47:23 +0700

    absensi lagi

--- src/pages/user/absen/AbsenMasuk.js
+++ src/pages/user/absen/AbsenMasuk.js
@@ -4,6 +4,7 @@ import Navbar from "../../../components/NavbarUser";
 import Webcam from "react-webcam";
 import axios from "axios";
 import Swal from "sweetalert2";
+import Loader from "../../../components/Loader";
 
 function AbsenMasuk() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -11,6 +12,8 @@ function AbsenMasuk() {
   const webcamRef = useRef(null);
   const [keteranganTerlambat, setKeteranganTerlambat] = useState("");
   const [error, setError] = useState("");
+  const userId = localStorage.getItem("userId");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -42,107 +45,114 @@ function AbsenMasuk() {
 
   const handleCaptureAndSubmit = async () => {
     const imageSrc = webcamRef.current.getScreenshot();
-    const formData = new FormData();
-    formData.append("image", dataURItoBlob(imageSrc));
-    formData.append("userId", localStorage.getItem("userId"));
-    formData.append("keteranganTerlambat", keteranganTerlambat);
-
+    const imageBlob = await imageSrc;
     try {
-      const response = await axios.post(
-        "http://localhost:2024/api/absensi/absensi-masuk",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-
-      Swal.fire({
-        position: "center",
-        icon: "success",
-        title: "Berhasil Absen",
-        showConfirmButton: false,
-        timer: 1500,
-      });
-      setTimeout(() => {
-        window.location.href = "/user/history_absen";
-      }, 1500);
+       const absensiCheckResponse = await axios.get(`http://localhost:2024/api/absensi/checkAbsensi/${userId}`);
+      const isUserAlreadyAbsenToday = absensiCheckResponse.data === "Pengguna sudah melakukan absensi hari ini.";
+      if (isUserAlreadyAbsenToday) {
+        Swal.fire("Info", "Anda sudah melakukan absensi hari ini.", "info");
+        setLoading(false);
+      } else {
+        const formData = new FormData();
+        formData.append("image", imageBlob);
+  
+         const response = await axios.post(
+          `http://localhost:2024/api/absensi/masuk/${userId}`,
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+  
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Berhasil Absen",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        setLoading(false);
+        setTimeout(() => {
+          window.location.href = "/user/history_absen";
+        }, 1500);
+      }
     } catch (err) {
       console.error("Error:", err);
       Swal.fire("Error", "Gagal Absen", "error");
+      setLoading(false);
+
     }
   };
 
-  function dataURItoBlob(dataURI) {
-    const byteString = atob(dataURI.split(",")[1]);
-    const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
-    const ab = new ArrayBuffer(byteString.length);
-    const ia = new Uint8Array(ab);
-    for (let i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-    }
-    return new Blob([ab], { type: mimeString });
-  }
+
+ 
   return (
-    <div className="flex flex-col h-screen">
-      <div className="sticky top-0 z-50">
-        <Navbar toggleSidebar={toggleSidebar} />
-      </div>
-      <div className="flex h-full">
-        <div className={`${sidebarOpen ? "fixed" : "hidden"} lg:flex`}>
-          <Sidebar isOpen={sidebarOpen} />
+    <>
+      {loading && <Loader />}
+      <div className="flex flex-col h-screen">
+        <div className="sticky top-0 z-50">
+          <Navbar toggleSidebar={toggleSidebar} />
         </div>
-        <div className="content-page max-h-screen container p-8 min-h-screen ml-0 lg:ml-64">
-          <div className="add-izin mt-12 bg-white p-5 rounded-xl shadow-lg border border-gray-300">
-            <h1 className="text-lg sm:text-2xl font-medium mb-4 sm:mb-7">
-              Absen Masuk
-            </h1>
-            <div className="text-base text-center mt-2">
-              {currentDateTime.toLocaleDateString("id-ID", {
-                weekday: "long",
-                day: "numeric",
-                month: "long",
-                year: "numeric",
-              })}{" "}
-              -{" "}
-              {tambahkanNolDepan(currentDateTime.getHours()) +
-                ":" +
-                tambahkanNolDepan(currentDateTime.getMinutes()) +
-                ":" +
-                tambahkanNolDepan(currentDateTime.getSeconds())}
+        <div className="flex h-full">
+          <div className={`${sidebarOpen ? "fixed" : "hidden"} lg:flex`}>
+            <Sidebar isOpen={sidebarOpen} />
+          </div>
+          <div className="content-page max-h-screen container p-8 min-h-screen ml-0 lg:ml-64">
+            <div className="add-izin mt-12 bg-white p-5 rounded-xl shadow-lg border border-gray-300">
+              <h1 className="text-lg sm:text-2xl font-medium mb-4 sm:mb-7">
+                Absen Masuk
+              </h1>
+              <div className="text-base text-center mt-2">
+                {currentDateTime.toLocaleDateString("id-ID", {
+                  weekday: "long",
+                  day: "numeric",
+                  month: "long",
+                  year: "numeric",
+                })}{" "}
+                -{" "}
+                {tambahkanNolDepan(currentDateTime.getHours()) +
+                  ":" +
+                  tambahkanNolDepan(currentDateTime.getMinutes()) +
+                  ":" +
+                  tambahkanNolDepan(currentDateTime.getSeconds())}
+              </div>
+              <div className="text-base text-center mt-2">{ucapan}</div>
+              {error && <div className="text-red-500">{error}</div>}
+              <form onSubmit={(e) => e.preventDefault()}>
+                <p className="font-bold text-center mt-8">Foto:</p>
+                <div className="flex justify-center">
+                  <Webcam audio={false} ref={webcamRef} />
+                </div>
+                <div className="flex justify-center mt-6">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      handleCaptureAndSubmit();
+                      setLoading(true);
+                    }}
+                    className="block w-32 sm:w-40 bg-blue-500 text-white rounded-lg py-3 text-sm sm:text-xs font-medium"
+                  >
+                    Ambil Foto
+                  </button>
+                </div>
+                <div className="relative mb-3 mt-5">
+                  <input
+                    type="text"
+                    id="keterangan_terlambat"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-5"
+                    placeholder="Keterangan Terlambat"
+                    value={keteranganTerlambat}
+                    onChange={(e) => setKeteranganTerlambat(e.target.value)}
+                  />
+                </div>
+              </form>
             </div>
-            <div className="text-base text-center mt-2">{ucapan}</div>
-            {error && <div className="text-red-500">{error}</div>}
-            <form onSubmit={(e) => e.preventDefault()}>
-              <p className="font-bold text-center mt-8">Foto:</p>
-              <div className="flex justify-center">
-                <Webcam audio={false} ref={webcamRef} />
-              </div>
-              <div className="flex justify-center mt-6">
-                <button
-                  type="button"
-                  onClick={handleCaptureAndSubmit}
-                   className="block w-32 sm:w-40 bg-blue-500 text-white rounded-lg py-3 text-sm sm:text-xs font-medium"
-                >
-                  Ambil Foto  
-                </button>
-              </div>
-              <div className="relative mb-3 mt-5">
-                <input
-                  type="text"
-                  id="keterangan_terlambat"
-                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-5"
-                  placeholder="Keterangan Terlambat"
-                  value={keteranganTerlambat}
-                  onChange={(e) => setKeteranganTerlambat(e.target.value)}
-                />
-              </div>
-            </form>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
